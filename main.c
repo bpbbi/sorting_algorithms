a3dc5513e4d048b63802026de3172054ec95f965
Artyom Sergeev <aosergeev@yandex.ru>
2025-07-27 19:25:59 +0300

    rename for convention case

--- main.c
+++ main.c
@@ -5,17 +5,17 @@
 #define SORT_IMPL
 #include "sort.h"
 
-typedef void (*sortFunc)(int*, int);
-#define ARRAY_CAPACITY 5
+typedef void (*SortFunc)(int*, int);
+#define ARRAY_CAPACITY 10000
 
-void generateRandomArray(int array[], int length) {
+void generate_random_array(int array[], int length) {
     srand(time(NULL));
     for(int i = 0; i < length; i++) {
         array[i] = rand() % 100;
     }
 }
 
-void printArr(int array[], int begin, int end) {
+void print_arrey(int array[], int begin, int end) {
     printf("[");
     for (int i = begin; i <= end; i++) {
         if (i > begin){
@@ -26,24 +26,24 @@ void printArr(int array[], int begin, int end) {
     printf("]\n");
 }
 
-double sortGetTime(int array[], int length, sortFunc f) {
+double sort_get_time(int array[], int length, SortFunc f) {
     clock_t start_time = clock();
     (*f)(array,length);
     clock_t end_time = clock();
-    double elapsed_time = (double)(end_time - start_time); // CLOCKS_PER_SEC;
+    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
     return elapsed_time;
 }
 
 int main(void) {
     int numbers[ARRAY_CAPACITY] = {0};
     size_t length = sizeof(numbers) / sizeof(numbers[0]);
-    generateRandomArray(numbers, length);
-    double time = sortGetTime(numbers, length, quickSort);
+    generate_random_array(numbers, length);
+    double time = sort_get_time(numbers, length, quick_sort);
 
     printf("Time to quick sort %d elements is %lf.\n", ARRAY_CAPACITY, time);
 
-    generateRandomArray(numbers, length);
-    time = sortGetTime(numbers, length, bubbleSort);
-    printf("Time to quick sort %d elements is %lf.\n", ARRAY_CAPACITY, time);
+    generate_random_array(numbers, length);
+    time = sort_get_time(numbers, length, bubble_sort);
+    printf("Time to bubble sort %d elements is %lf.\n", ARRAY_CAPACITY, time);
     return 0;
 }
--- sort.h
+++ sort.h
@@ -1,28 +1,28 @@
-void bubbleSort(int array[], int length);
-void quickSort(int array[], int length);
+void bubble_sort(int array[], int length);
+void quick_sort(int array[], int length);
 
 #ifdef SORT_IMPL
-void swapElemsByIndexes(int array[],int index1, int index2) {
+void swap_elements_by_indexes(int array[],int index1, int index2) {
     if (index1 == index2) return;
     int tmp = array[index1];
     array[index1] = array[index2];
     array[index2] = tmp;
 }
 
-void bubbleSort(int array[], int length) {
+void bubble_sort(int array[], int length) {
     bool sorted;
     do {
         sorted = true;
         for(int i = 1; i <= length; ++i) {
             if(array[i-1] > array[i]) {
                 sorted = false;
-                swapElemsByIndexes(array, i-1, i);
+                swap_elements_by_indexes(array, i-1, i);
             }
         }
     } while(!sorted);
 }
 
-void quickSortImp(int array[], int begin, int end) {
+void quick_sort_imp(int array[], int begin, int end) {
     if (begin >= end) return;
     const int medVal= array[end];
     int i = begin;
@@ -31,16 +31,16 @@ void quickSortImp(int array[], int begin, int end) {
         if (medVal >= array[i]){
             ++i;
         } else {
-            swapElemsByIndexes(array,i,j-1);
-            swapElemsByIndexes(array,j,j-1);
+            swap_elements_by_indexes(array,i,j-1);
+            swap_elements_by_indexes(array,j,j-1);
             --j;
         }
     }
-    quickSortImp(array,begin, j - 1);
-    quickSortImp(array, j, end);
+    quick_sort_imp(array,begin, j - 1);
+    quick_sort_imp(array, j, end);
 }
 
-void quickSort(int array[], int length) {
-    quickSortImp(array,0,length - 1);
+void quick_sort(int array[], int length) {
+    quick_sort_imp(array,0,length - 1);
 }
 #endif
